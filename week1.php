8f9b0a5a99a3ab429b71ea6c2a6f4a8c33011d13
fnyfh <90744361+fnyfh@users.noreply.github.com>
2021-09-15 14:20:47 +0700

    Create week1.php

---
+++ week1.php
@@ -0,0 +1,34 @@
+<?php
+$nama = "Fanny";
+$kelas = "MI20B";
+$jurusan = "Manajemen Informatika";
+$nohp = "08122145678";
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>WEEK 1 - Fanny Fietya</title>
+</head>
+
+<body>
+  <?php
+  echo "================";
+  echo "<br>";
+  echo "BIODATA";
+  echo "<br>";
+  echo "================";
+  echo "<br>";
+  echo "<br>";
+  echo "Nama : " . $nama;
+  echo "<br>";
+  echo "Kelas : " . $kelas;
+  echo "<br>";
+  echo "Jurusan : " . $jurusan;
+  echo "<br>";
+  echo "NO HP : " . $nohp;
+  ?>
+
+</body>
+
+</html>
