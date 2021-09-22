e137aea1b4b3254a74660bbb39d56dbe260dc563
fnyfh <90744361+fnyfh@users.noreply.github.com>
2021-09-22 10:28:26 +0700

    Create array.php

---
+++ array.php
@@ -0,0 +1,14 @@
+<?php
+  $biodata = array(
+    'Nama' => 'Fanny Fietya Herlambang',
+    'Kelas' => 'MI20B',
+    'Jurusan' => 'Manajemen Informatika',
+    'NO HP' => '08122145678',
+  );
+  echo $biodata['Nama'];
+  echo "<br>";
+  echo $biodata['Kelas'];
+  echo "<br>";
+  echo $biodata['Jurusan'];
+  echo "<br>";
+  echo $biodata['NO HP'];
