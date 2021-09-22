e7a421e066aeb7852ee68e5ac8249170d5f0e2f2
fnyfh <90744361+fnyfh@users.noreply.github.com>
2021-09-22 10:34:58 +0700

    Update array.php

--- array.php
+++ array.php
@@ -1,10 +1,27 @@
 <?php
-  $biodata = array(
-    'Nama' => 'Fanny Fietya Herlambang',
-    'Kelas' => 'MI20B',
-    'Jurusan' => 'Manajemen Informatika',
-    'NO HP' => '08122145678',
-  );
+$biodata = array(
+  'Nama' => 'Fanny Fietya Herlambang',
+  'Kelas' => 'MI20B',
+  'Jurusan' => 'Manajemen Informatika',
+  'NO HP' => '08122145678',
+);
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Array - Fanny Fietya</title>
+</head>
+
+<body>
+  <?php
+  echo "================";
+  echo "<br>";
+  echo "BIODATA";
+  echo "<br>";
+  echo "================";
+  echo "<br>";
+  echo "<br>";
   echo $biodata['Nama'];
   echo "<br>";
   echo $biodata['Kelas'];
@@ -12,3 +29,7 @@
   echo $biodata['Jurusan'];
   echo "<br>";
   echo $biodata['NO HP'];
+  ?>
+</body>
+
+</html>
