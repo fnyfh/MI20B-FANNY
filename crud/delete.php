a3d9b47773354c5eba9330e33b57f48832a0037a
fnyfh <90744361+fnyfh@users.noreply.github.com>
2021-10-27 14:17:47 +0700

    Create delete.php

---
+++ crud/delete.php
@@ -0,0 +1,11 @@
+<?php
+
+$con = new mysqli("localhost", "root", "", "dbsuratfanny");
+
+
+if (isset($_POST['delete'])) {
+  $id = $_POST['id'];
+
+  $result = mysqli_query($con, "DELETE FROM `tbl_surat` WHERE `tbl_surat`.`id` = $id");
+  header("Location:view2.php");
+}
