040da22d3c9d668329edb4a3ded4b64346c4d435
fnyfh <90744361+fnyfh@users.noreply.github.com>
2021-11-17 14:14:50 +0700

    Update delete.php

--- crud/delete.php
+++ crud/delete.php
@@ -7,5 +7,5 @@ if (isset($_POST['delete'])) {
   $id = $_POST['id'];
 
   $result = mysqli_query($con, "DELETE FROM `tbl_surat` WHERE `tbl_surat`.`id` = $id");
-  header("Location:view2.php");
+  header("Location:view2.php?pesan=success&&frm=del");
 }
