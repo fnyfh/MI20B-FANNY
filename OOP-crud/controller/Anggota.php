ff8ecd11f3e36e861a6d4df4ed5f0a1214d853b7
fnyfh <90744361+fnyfh@users.noreply.github.com>
2021-11-17 14:13:42 +0700

    Update Anggota.php

--- OOP-crud/controller/Anggota.php
+++ OOP-crud/controller/Anggota.php
@@ -13,10 +13,59 @@ class Anggota {
 	}
 
 	function index(){
-
 		$hasil = $this->model->tampil_data();
-
 	return $hasil;
+	}
+	function getData($id){
+		$hasil = $this->model->getData($id);
+	return $hasil;
+	}
+	function getJenisData(){
+		$hasil = $this->model->getJenisData();
+	return $hasil;
+	}
+	function getJenisData2(){
+		$hasil = $this->model->getJenisData2();
+	return $hasil;
+	}
+	function hapusData(){
+		if (isset($_POST['delete'])) {
+			$id = $_POST['id'];
+			$result = $this->model->hapusData($id);
+
+			if ($result) {
+				header("Location:content.php?pesan=success&frm=del");
+			}else{
+				header("Location:content.php?pesan=gagal&frm=del");
+			}
+		}
+	}
+	function simpanData(){
+		if(isset($_POST['simpan'])){
+			$nim = $_POST['nim'];
+			$nama = $_POST['nama'];
+			$noHp = $_POST['no_hp'];
+			$alamat = $_POST['alamat'];
+			$jurusan = $_POST['jurusan'];
+			$jabatan = $_POST['jabatan'];
+
+			$data[] = array(
+				'nim' => $nim,
+				'nama' => $nama,
+				'no_hp' => $noHp,
+				'alamat' => $alamat,
+				'jurusan' => $jurusan,
+				'jabatan' => $jabatan,
+			);
+
+			$result = $this->model->simpanData($data);
+
+			if ($result) {
+				header("Location:content.php?pesan=success&frm=add");
+			}else{
+				header("Location:content.php?pesan=gagal&frm=add");
+			}
+		}
 	}
 }
 
