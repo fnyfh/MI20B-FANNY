4844d4c2b710dbcdabfb9b2a312aad3eb95e5c1a
fnyfh <90744361+fnyfh@users.noreply.github.com>
2021-11-17 14:13:56 +0700

    Update Anggota_model.php

--- OOP-crud/model/Anggota_model.php
+++ OOP-crud/model/Anggota_model.php
@@ -12,6 +12,63 @@ class Anggota_model{
 		$row->execute();
 		return $hasil = $row->fetchAll();
 	}
-	
+	function getData($id){
+		$row = $this->db->prepare("SELECT * FROM tbl_anggota where id = $id");
+		$row->execute();
+		return $hasil = $row->fetch();
+	}
+	function getJenisData(){
+		$row = $this->db->prepare("SELECT * FROM tbl_jurusan");
+		$row->execute();
+		return $hasil = $row->fetchAll();
+	}
+	function getJenisData2(){
+		$row = $this->db->prepare("SELECT * FROM tbl_jabatan");
+		$row->execute();
+		return $hasil = $row->fetchAll();
+	}
+	function simpanData($data){
+		//buat array untuk isi value insert sumber kode
+		$rowsSQL = array();
+		//buat untuk param prepared Statement
+		$toBind = array();
+		//list nama kolom
+		$columnNames = array_keys($data[0]);
+		//looping untuk mengambil isi dari kolom/values
+		foreach ($data as $arrayIndex => $row) {
+			$params = array();
+			foreach($row as $columnNames => $columnValue){
+				$param = ":".$columnNames.$arrayIndex;
+				$params[] = $param;
+				$toBind[$param] = $columnValue;
+			}
+			$rowsSQL[] = "(".implode(",", $param).")";
+		}
+		$sql = "INSERT INTO tbl_anggota (".implode(",", $columnNames).") VALUES".implode(",", $rowsSQL);
+		$row = $this->db->prepare($sql);
+		//Bind our values.
+		foreach($toBind as $param => $val){
+			$row->bindValue($param, $val);
+		}
+	}
+	function updateData($data, $id){
+		$setPart =array();
+		foreach ($data as $key => $value) {
+			$setPart[] = $key."=:".$key;
+		}
+		$sql = "UPDATE tbl_anggota SET ".implode(',', $setPart)."WHERE id = :id";
+		$row = $this->db->prepare($sql);
+		//bind our values
+		$row->bindValue(':id',$id); //where
+		foreach ($data as $param => $val) {
+			$row->bindValue($param,$val);
+		}
+		return $row->execute();
+	}
+	function hapusData($id){
+		$sql = "DELETE FROM tbl_anggota WHERE id = ?";
+		$row = $this->db->prepare($sql);
+		return $row ->execute(array($id));
+	}
 }
 ?>
