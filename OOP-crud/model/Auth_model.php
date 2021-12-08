458136b4a76a98272de5ac7758f482e287c06366
Firman <90744361+fnyfh@users.noreply.github.com>
2021-12-08 14:05:12 +0700

    Update Auth_model.php

--- OOP-crud/model/Auth_model.php
+++ OOP-crud/model/Auth_model.php
@@ -9,7 +9,7 @@ class Auth_model {
 
     function proses_login($user,$pass)
     {
-        // untuk password kita enkrip dengan md5
+        // untuk password kita enskrip dengan md5
         $row = $this->db->prepare('SELECT * FROM tbl_user WHERE username=? AND password=md5(?)');
         $row->execute(array($user,$pass));
         $count = $row->rowCount();
