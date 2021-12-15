d1b3e3e468e923533902f9f76f2914d077e7ec1c
Firman <90744361+fnyfh@users.noreply.github.com>
2021-12-15 14:18:24 +0700

    Update add.php

--- OOP-crud/view/add.php
+++ OOP-crud/view/add.php
@@ -3,7 +3,7 @@ error_reporting(0);
 include '../controller/Anggota.php';
 
 $ctrl = new Anggota();
-$query2 = $ctrl->getJenisData($id);
+/*$query2 = $ctrl->getJenisData($id);*/
 $query3 = $ctrl->getJenisData2($id);
 
 
@@ -20,10 +20,94 @@ $query3 = $ctrl->getJenisData2($id);
   <title>Tambah Anggota</title>
   <!-- <link rel="stylesheet" href="../asset/css/bootstrap.min.css"> -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+
 </head>
 
 <body>
+  <script type="text/javascript">
+    $(document).ready(function(){
+      // alert('test');
+      show_jurusan();
+
+      function show_jurusan(){
+        $.ajax({
+          type  : 'GET',
+          url   : 'api.php',
+          async : false,
+          dataType  : 'json',
+          success : function(data){
+            console.log(data);
+            var html = '';
+            var i;
+            var no;
+            for (i=0; i<data.length; i++){
+              no = i + 1;
+              html +=
+                '<option value="'+data[i].id_jurusan+'">'+data[i].jurusan+'</option>';
+            }
+            $('#jurusan').html(html);
+          },
+          error:function(data){
+            console.log(data);
+          }
+        });
+      }
+
+    $('#ModalJenis').modal('hide');
+    var dataForm = $('#formJenisSurat').serialize();
+    $.ajax({
+    type  : 'POST',
+    url   : 'api.php',//Memanggil Controller/Function
+    async : false,
+    dataType : 'json',
+    data : dataForm, 
+    success:function(response){
+            if (response == 200) {
+          Swal.fire({
+          type: 'success',
+          title: 'Berhasil di simpan!',
+          text: 'Tunggu sejenak',
+          timer: 1000,
+          showCancelButton: false,
+          showConfirmButton: false
+          })
+          .then (function() {
+            show_jenis();
+          });
+
+            } else {
+
+                Swal.fire({
+                type: 'error',
+                title: 'Gagal menyimpan',
+                text: 'silahkan coba lagi!'
+              });
+
+            }
+
+            console.log(response);
+
+          },
+
+          error:function(response){
+
+              Swal.fire({
+                type: 'error',
+                title: 'Opps!',
+                text: 'server error!'
+              });
+
+              console.log(response);
+
+          }
+    });
+
+  });  
+  </script>
   <div class="container">
     <row>
       <div class="card">
@@ -50,14 +134,10 @@ $query3 = $ctrl->getJenisData2($id);
               <label for="jurusan" class="form-label">Jurusan</label>
               <select id="jurusan" class="form-select" name="jurusan" required>
                 <option selected>Masukan Pilihan</option>
-                <?php
-                foreach ($query2 as $jurusan) {
-                ?>
-                  <option value="<?= $jurusan['id_jurusan'] ?>"><?= $jurusan['jurusan'] ?></option>
-                <?php
-                }
-                ?>
               </select>
+            </div>
+            <div class="col-md-6">
+              <a href="#" class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#jurusan" ><i class="bi bi-plus"></i></a>
             </div>
             <div class="col-md-6">
               <label for="jabatan" class="form-label">Jabatan</label>
@@ -81,6 +161,27 @@ $query3 = $ctrl->getJenisData2($id);
       </div>
     </row>
   </div>
+  <div class="example-modal">
+        <div id="modalData" class="modal fade" role="dialog" style="display: none;">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <form class="row g-3" id="formJurusan">
+                <div class="modal-header">
+                  <h3 class="modal-title">Tambah Jurusan</h3>
+                </div>
+                <div class="modal-body">
+                  <label for="jurusan" class="form-label">Jurusan</label>
+                  <input type="text" class="form-control" id="jurusan" name="Jurusan" placeholder="Jurusan">
+                </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-danger btn-block" id="btnSimpan">Simpan</button>
+                    <button type="button" class="btn btn-primary pull-left" data-bs-dismiss="modal">Cancel</button>
+                  </div>
+              </form>
+            </div>
+          </div>
+        </div>
+    </div>
   
 </body>
 <!-- <script src="../asset/js/bootstrap.min.js"></script> -->
